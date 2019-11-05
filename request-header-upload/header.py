600395080fa1cf6b8886e6bed6a9e3545c709239
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-11-05 12:41:23 -0600

    Update header.py

--- request-header-upload/header.py
+++ request-header-upload/header.py
@@ -1,13 +1,13 @@
-from botocore.exceptions import ClientError
+rom botocore.exceptions import ClientError
 import requests
 import boto3
 import json
 import boto3
 import os
 
-url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+url = " https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
 file_name = "header.txt"
-bucket_name = "marvel-scripting-assessment-bucket"
+bucket_name = "dilfuza-marvel-task"
 
 if os.environ.get("ACCESS_KEY") and os.environ.get("SECRET_KEY"):
     creds = {
@@ -27,21 +27,21 @@ def getHeaderLoadFile(url, file_name):
 
     resp = requests.get(url)
     
-    # checking for response code
+    
     if resp.status_code == 200:
 
-        # open the file write mode
+        
         with open(file_name, 'w') as file:
             file.write(f"{resp.headers}")
 
 
 def upload_file(file_name, bucket, object_name=None):
 
-    # If S3 object_name was not specified, use file_name
+
     if object_name is None:
         object_name = file_name
 
-    # Upload the file
+    
     s3_client = boto3.client(
         's3',
         aws_access_key_id=creds['aws_access_key_id'],
@@ -56,8 +56,8 @@ def upload_file(file_name, bucket, object_name=None):
 
 if __name__ == '__main__':
 
-    # getHeaderLoadFile should be called first
+    
     getHeaderLoadFile(url, file_name)
 
-    # upload_file function should be called after getHeaderLoadFile
+    
     upload_file(file_name, bucket_name)
